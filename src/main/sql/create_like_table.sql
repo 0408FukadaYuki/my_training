8bb3ba07b52989a75e975cd9b5a03a3626ad7b4b
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-05 13:58:35 +0900

    シングルクォートからバッククォートに変更

--- src/main/sql/create_like_table.sql
+++ src/main/sql/create_like_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE
-    'like' (
-        'user_id' VARCHAR(15) NOT NULL,
-        'post_id' int NOT NULL,
-        'created_at' DATETIME,
+    `like` (
+        `user_id` VARCHAR(15) NOT NULL,
+        `post_id` int NOT NULL,
+        `created_at` DATETIME,
         PRIMARY KEY (user_id, post_id),
         FOREIGN KEY (user_id) REFERENCES user (id),
         FOREIGN KEY (post_id) REFERENCES post (id),
--- src/main/sql/create_user_table.sql
+++ src/main/sql/create_user_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE
-    'user' (
-        'id' int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        'user_id' VARCHAR(15) NOT NULL,
-        'name' VARCHAR(30) NOT NULL,
-        'mail' VARCHAR(50) NOT NULL,
-        'profile' VARCHAR(100),
-        'birth_date' DATE,
-        'icon_image' BLOB,
-        'password' CHAR(64),
+    `user` (
+        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_id` VARCHAR(15) NOT NULL,
+        `name` VARCHAR(30) NOT NULL,
+        `mail` VARCHAR(50) NOT NULL,
+        `profile` VARCHAR(100),
+        `birth_date` DATE,
+        `icon_image` BLOB,
+        `password` CHAR(64),
     );
