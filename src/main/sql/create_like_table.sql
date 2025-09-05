7aa7fd48faaf5d2deb279e70ad85d1e7cd9f4b9a
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-05 11:32:33 +0900

    LikeテーブルのCreate作成

---
+++ src/main/sql/create_like_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+    'like' (
+        'user_id' VARCHAR(15) NOT NULL,
+        'post_id' int NOT NULL,
+        'created_at' DATETIME,
+        PRIMARY KEY (user_id, post_id),
+        FOREIGN KEY (user_id) REFERENCES user (id),
+        FOREIGN KEY (post_id) REFERENCES post (id),
+    );
