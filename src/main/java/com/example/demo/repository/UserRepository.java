7b6a8ab6feebc8488c33345dfffd134ec9220708
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-08 15:37:14 +0900

    キーをuuidに変更したため、LongからStringに型を修正

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.User;
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
