1ef7a874484aec9a3dc1aa35869ad07e14755147
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-08 13:20:48 +0900

    PostsテーブルのEntityを作成

---
+++ src/main/java/com/example/demo/model/Post.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "uuid", nullable = false)
+    private User userId;
+    @Column(name = "content", length = 140)
+    private String content;
+    @Column(name = "reply_to")
+    private Long replyTo;
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Post() {
+    }
+}
