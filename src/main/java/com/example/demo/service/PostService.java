c9c47272aa8ecb93f3731ee69c9ef45994509706
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-09 14:25:53 +0900

    PostServiceの新規作成

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.model.request.CreatePostRequest;
+
+public interface PostService {
+    public void createPost(CreatePostRequest createPostRequest);
+}
