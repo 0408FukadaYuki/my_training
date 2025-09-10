99c2fdb0fc9c3ccaf78fe13028ff4431e71b6e53
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-10 13:56:58 +0900

    DBの振り分け実装の成功

--- src/test/java/com/example/demo/UserRepositoryTest.java
+++ src/test/java/com/example/demo/UserRepositoryTest.java
@@ -20,7 +20,9 @@ import com.example.demo.repository.UserRepository;
 // TODO: 開発用とテスト用とで接続先DBを分ける
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
-@Sql(scripts = "/test-data.sql")
+
+@Sql(scripts = "classpath:testdata/test-data.sql", 
+	executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class UserRepositoryTest {
 
 	@Autowired
@@ -29,11 +31,11 @@ class UserRepositoryTest {
 	@Test
 	void getUserById() throws Exception {
 		User expectUser = new User();
-		expectUser.setUuid("123e4567-e89b-12d3-a456-426614174000");
-		expectUser.setUserId("suzuki_tarou");
-		expectUser.setName("鈴木 太郎");
-		expectUser.setMail("tanaka@example.com");
-		expectUser.setProfile("おはよう");
+		expectUser.setUuid("a23e4567-e89b-12d3-a456-426614174000");
+		expectUser.setUserId("test_tarou");
+		expectUser.setName("テスト 太郎");
+		expectUser.setMail("tarou@example.com");
+		expectUser.setProfile("テストです");
 		String dateString = "1985-05-15 00:00:00.0";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
 		Date date = sdf.parse(dateString);
@@ -41,7 +43,7 @@ class UserRepositoryTest {
 		expectUser.setIconImage("");
 		expectUser.setPassword("ef92b778bafe771e89245b89ecbcf23e9a8a0d53a71f80626e3e408559caa046");
 
-		Optional<User> user = userRepository.findById("123e4567-e89b-12d3-a456-426614174000");
+		Optional<User> user = userRepository.findById("a23e4567-e89b-12d3-a456-426614174000");
         User actualUser = user.get();
 		assertEquals(expectUser, actualUser);
 
