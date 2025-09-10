be7e0a37b47b0341dde7febab8cb95abfa933ee7
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-10 16:44:15 +0900

    新規投稿失敗時のカスタム例外クラスの作成

---
+++ src/main/java/com/example/demo/exception/PostNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class PostNotCreatedException extends RuntimeException {
+    public PostNotCreatedException(String message) {
+        super(message);
+    }
+}
