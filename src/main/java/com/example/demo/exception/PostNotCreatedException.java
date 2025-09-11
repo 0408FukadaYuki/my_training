831b585c433d3c48908c36d1cc00c7d17abd6baa
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-11 11:17:33 +0900

    javadocコメントを追加

--- src/main/java/com/example/demo/exception/PostNotCreatedException.java
+++ src/main/java/com/example/demo/exception/PostNotCreatedException.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
 
+/**
+ * 新規投稿の際、DBアクセスエラー発生時に使用するカスタムクラス
+ */
 public class PostNotCreatedException extends RuntimeException {
     public PostNotCreatedException(String message) {
         super(message);
