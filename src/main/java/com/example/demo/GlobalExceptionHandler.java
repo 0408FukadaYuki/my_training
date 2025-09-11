f2096d06efbef507afd8dc72b64559deefe648f9
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-11 11:17:13 +0900

    importの修正

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.demo.exception.PostNotCreatedException;
+import com.example.demo.model.ErrorResponseBody;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
