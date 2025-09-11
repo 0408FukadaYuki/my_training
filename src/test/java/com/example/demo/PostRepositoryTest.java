159f660b9f8b4cce99272696fd2c5584bbee6481
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-11 10:19:35 +0900

    期待値とassertEqualsの修正

--- src/test/java/com/example/demo/PostRepositoryTest.java
+++ src/test/java/com/example/demo/PostRepositoryTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
@@ -25,12 +28,12 @@ class PostRepositoryTest {
 	@DBRider
 	void testCreatePost() throws Exception {
         User user = new User();
-        user.setUuid("123e4567-e89b-12d3-a456-426614174000");
+        user.setUuid("a23e4567-e89b-12d3-a456-426614174000");
 
         //期待値の作成
 		Post expectPost = new Post();
         expectPost.setContent("これはPost作成のテストです");
-        expectPost.setId((long)4);
+        expectPost.setId((long)7);
         expectPost.setUserId(user);
         LocalDateTime now = LocalDateTime.now();
         expectPost.setCreatedAt(now);
@@ -41,13 +44,12 @@ class PostRepositoryTest {
         createPostInfo.setUserId(user);
         postRepository.save(createPostInfo);
 
-		Optional<Post> post = postRepository.findById((long)4);
+		Optional<Post> post = postRepository.findById((long)7);
         Post resultPost = post.get();
 		assertEquals(expectPost.getId(), resultPost.getId());
-        assertEquals(expectPost.getUserId(), resultPost.getUserId());
+        assertEquals(expectPost.getUserId().getUuid(), resultPost.getUserId().getUuid());
         assertEquals(expectPost.getReplyTo(), resultPost.getReplyTo());
         assertEquals(expectPost.getContent(), resultPost.getContent());
-        // ミリ秒単位の時間がずれてしまう
-        // assertEquals(expectPost.getCreatedAt(), resultPost.getCreatedAt());
+        assertEquals(expectPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS), resultPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS));
 	}
 }
