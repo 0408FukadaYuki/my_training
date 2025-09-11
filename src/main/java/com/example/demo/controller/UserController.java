49644b7307310860fc363f062cce0db1c5ff2db7
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-11 13:40:46 +0900

    不要なimportを削除

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserServiceImpl;
 import com.example.demo.model.CreateUserRequest;
-import com.example.demo.model.User;
-import com.example.demo.model.UserResponse;
-import com.example.demo.model.UpdateUserRequest;
-
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
