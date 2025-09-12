532330b730576cd5b0c9d29d6c83805aae690701
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-12 14:23:06 +0900

    仮実装だったリクエストモデルを修正

--- src/main/java/com/example/demo/model/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/CreateUserRequest.java
@@ -1,10 +1,21 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
 import lombok.Data;
 
+/**
+ * 新規ユーザー作成APIに使用するリクエストモデル
+ */
 @Data
 public class CreateUserRequest {
+    private String userId;
     private String name;
-    private int age;
-    public CreateUserRequest(){}
+    private String mail;
+    private String profile;
+    private LocalDate birthDate;
+    private String iconImage;
+    private String password;
+
+    public CreateUserRequest() {
+    }
 }
