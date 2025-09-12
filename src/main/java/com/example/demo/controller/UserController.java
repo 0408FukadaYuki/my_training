2c4ff64f1e8f3a7805859d83db8c228a870f4e73
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-12 09:14:13 +0900

    @RequestMappingを追加

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,8 +7,10 @@ import com.example.demo.service.UserServiceImpl;
 import com.example.demo.model.CreateUserRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
