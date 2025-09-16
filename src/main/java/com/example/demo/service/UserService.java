761dacb36698ffa705ae56b4659d03d56a32e94a
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-16 16:20:46 +0900

    リクエストモデルをmodel/request配下に移動

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.CreateUserRequest;
+import com.example.demo.model.request.CreateUserRequest;
 
 public interface UserService {
     /**
