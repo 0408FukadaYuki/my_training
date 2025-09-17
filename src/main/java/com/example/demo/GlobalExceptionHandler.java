073f40f44c67494c1ad3e1eb59efac69deb1d9ca
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-17 11:48:22 +0900

    お気に入り登録の際の例外ハンドリングを実装

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -5,10 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.demo.exception.FavoriteNotCreatedException;
 import com.example.demo.exception.PostNotCreatedException;
 import com.example.demo.exception.PostNotGetException;
 import com.example.demo.model.ErrorResponseBody;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -22,6 +22,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(PostNotGetException.class)
     public ResponseEntity<ErrorResponseBody> handlePostNotGetException(PostNotCreatedException e) {
+        ErrorResponseBody errorResponseBody = new ErrorResponseBody();
+        errorResponseBody.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponseBody.setErrorMessage(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseBody);
+    }
+
+    @ExceptionHandler(FavoriteNotCreatedException.class)
+    public ResponseEntity<ErrorResponseBody> handleFavoriteNotGetException(FavoriteNotCreatedException e) {
         ErrorResponseBody errorResponseBody = new ErrorResponseBody();
         errorResponseBody.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         errorResponseBody.setErrorMessage(e.getMessage());
