784a9e09e6a6a8f7519e9dd4ff7f5e52e683ba10
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-17 16:54:42 +0900

    投稿取得のテストケースを追加

--- src/test/java/com/example/demo/PostRepositoryTest.java
+++ src/test/java/com/example/demo/PostRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import com.github.database.rider.spring.api.DBRider;
 import com.example.demo.model.Post;
@@ -17,39 +19,59 @@ import com.example.demo.repository.PostRepository;
 
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.yml")
-@Sql(scripts = "classpath:testdata/test-data.sql", 
-	executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "classpath:testdata/test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class PostRepositoryTest {
 
-	@Autowired
-	private PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-	@Test
-	@DBRider
-	void testCreatePost() throws Exception {
+    @Test
+    @DBRider
+    void testCreatePost() throws Exception {
         User user = new User();
         user.setUuid("a23e4567-e89b-12d3-a456-426614174000");
 
-        //期待値の作成
-		Post expectPost = new Post();
+        // 期待値の作成
+        Post expectPost = new Post();
         expectPost.setContent("これはPost作成のテストです");
-        expectPost.setId((long)7);
+        expectPost.setId((long) 7);
         expectPost.setUserId(user);
         LocalDateTime now = LocalDateTime.now();
         expectPost.setCreatedAt(now);
-        
 
         Post createPostInfo = new Post();
         createPostInfo.setContent("これはPost作成のテストです");
         createPostInfo.setUserId(user);
         postRepository.save(createPostInfo);
 
-		Optional<Post> post = postRepository.findById((long)7);
+        Optional<Post> post = postRepository.findById((long) 7);
         Post resultPost = post.get();
-		assertEquals(expectPost.getId(), resultPost.getId());
+        assertEquals(expectPost.getId(), resultPost.getId());
         assertEquals(expectPost.getUserId().getUuid(), resultPost.getUserId().getUuid());
         assertEquals(expectPost.getReplyTo(), resultPost.getReplyTo());
         assertEquals(expectPost.getContent(), resultPost.getContent());
-        assertEquals(expectPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS), resultPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS));
-	}
+        assertEquals(expectPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS),
+                resultPost.getCreatedAt().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @Test
+    @DBRider
+    void testFindAllPost() throws Exception {
+        Iterable<Post> response = postRepository.findAll();
+        List<Post> actual = new ArrayList<>();
+        response.forEach(actual::add);
+        assertEquals(6, actual.size());
+        assertEquals(1, actual.get(0).getId());
+        assertEquals("123e4567-e89b-12d3-a456-426614174000", actual.get(0).getUserId().getUuid());
+        assertEquals("初めての投稿です。よろしくお願いします。", actual.get(0).getContent());
+        assertEquals(null, actual.get(0).getReplyTo());
+        assertEquals(2, actual.get(1).getId());
+        assertEquals("223e4567-e89b-12d3-a456-426614174001", actual.get(1).getUserId().getUuid());
+        assertEquals("初めての投稿です。よろしくお願いします。", actual.get(1).getContent());
+        assertEquals(null, actual.get(1).getReplyTo());
+        assertEquals(3, actual.get(2).getId());
+        assertEquals("123e4567-e89b-12d3-a456-426614174000", actual.get(2).getUserId().getUuid());
+        assertEquals("2回目の投稿です。", actual.get(2).getContent());
+        assertEquals(null, actual.get(2).getReplyTo());
+    }
 }
