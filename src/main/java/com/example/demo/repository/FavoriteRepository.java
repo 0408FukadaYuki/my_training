029e68dc5fb93c951e1b695deb94dbc63a0e80b7
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-18 10:51:47 +0900

    uuidで検索するための関数を追加

--- src/main/java/com/example/demo/repository/FavoriteRepository.java
+++ src/main/java/com/example/demo/repository/FavoriteRepository.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Favorite;
 import com.example.demo.model.FavoritePK;
+import com.example.demo.model.User;
+
+import java.util.List;
+
 
 @Repository
 public interface FavoriteRepository extends CrudRepository<Favorite, FavoritePK> {
+    List<Favorite> findByUser(User user);
 }
