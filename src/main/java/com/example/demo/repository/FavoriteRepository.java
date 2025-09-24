9a1527f8dac6067055cc1cd981ace32c479036b2
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-24 11:16:14 +0900

    返却値の型をIterableに変更

--- src/main/java/com/example/demo/repository/FavoriteRepository.java
+++ src/main/java/com/example/demo/repository/FavoriteRepository.java
@@ -7,9 +7,7 @@ import com.example.demo.model.Favorite;
 import com.example.demo.model.FavoritePK;
 import com.example.demo.model.User;
 
-import java.util.List;
-
 @Repository
 public interface FavoriteRepository extends CrudRepository<Favorite, FavoritePK> {
-    List<Favorite> findByUser(User user);
+    Iterable<Favorite> findByUser(User user);
 }
