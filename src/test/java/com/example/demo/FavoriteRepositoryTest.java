1d55fad5bc5d0d069a0f4f9d1791800ba2875dcc
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-29 10:23:15 +0900

    テストに使用に使用されていない不要な項目を削除

--- src/test/java/com/example/demo/FavoriteRepositoryTest.java
+++ src/test/java/com/example/demo/FavoriteRepositoryTest.java
@@ -7,13 +7,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.time.LocalDateTime;
 import com.github.database.rider.spring.api.DBRider;
 import com.example.demo.model.Favorite;
 import com.example.demo.model.FavoritePK;
 import com.example.demo.model.Post;
 import com.example.demo.model.User;
-import com.example.demo.model.request.CreateFavoriteRequest;
 import com.example.demo.repository.FavoriteRepository;
 
 @DataJpaTest
@@ -27,33 +25,17 @@ class FavoriteRepositoryTest {
     @Test
     @DBRider
     void testFindByUser() throws Exception {
-        LocalDateTime localDateTime = LocalDateTime.of(1990, 1, 1, 0, 0, 0);
         Favorite expectavorite = new Favorite();
         Post post = new Post();
         post.setId((long) 2);
-        post.setContent("これはJunitのテストです");
-        post.setReplyTo(null);
-        post.setCreatedAt(localDateTime);
-        User postUser = new User();
-        postUser.setUuid("b23e4567-e89b-12d3-a456-426614174000");
-        postUser.setUserId("test_hanako");
-        postUser.setName("テスト 花子");
-        post.setUserId(postUser);
         FavoritePK favoritePK = new FavoritePK();
         favoritePK.setPostId((long) 2);
         favoritePK.setUserId("a23e4567-e89b-12d3-a456-426614174000");
         User user = new User();
         user.setUuid("a23e4567-e89b-12d3-a456-426614174000");
-        user.setUserId("test_tarou");
-        user.setName("テスト 太郎");
         expectavorite.setFavoritePK(favoritePK);
         expectavorite.setPost(post);
         expectavorite.setUser(user);
-        expectavorite.setCreatedAt(localDateTime);
-
-        CreateFavoriteRequest createFavoriteRequest = new CreateFavoriteRequest();
-        createFavoriteRequest.setUuid("a23e4567-e89b-12d3-a456-426614174000");
-        createFavoriteRequest.setPostId((long) 5);
 
         User findUser = new User();
         findUser.setUuid("a23e4567-e89b-12d3-a456-426614174000");
