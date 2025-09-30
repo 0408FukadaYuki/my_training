aebba419d62d3cba447d20630dca5acfbff7a54f
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-09-30 13:42:50 +0900

    createFavoriteが呼ばれているかをverifyで検証するコードを追加

--- src/test/java/com/example/demo/FavoriteControllerTest.java
+++ src/test/java/com/example/demo/FavoriteControllerTest.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -50,6 +53,7 @@ class FavoriteControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(request))
                 .andExpect(status().isOk());
+        verify(favoriteService, times(1)).createFavorite(createFavoriteRequest);
     }
 
     @Test
