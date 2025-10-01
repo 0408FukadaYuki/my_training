1516e2e0f62942b415fd59ad45ef8fdf51ba4b09
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-10-01 14:43:47 +0900

    投稿が取得できなかった場合、エラーアラートを表示するように修正

--- frontend/app/pages/timeLine.vue
+++ frontend/app/pages/timeLine.vue
@@ -2,20 +2,31 @@
 const { getPost } = usePost()
 const postStore = usePostStore();
 
+const state = reactive({
+    showAlertFlag: false,
+    alertMessage: "",
+})
+
 onMounted(async () => {
     try {
         const posts: Post[] = await getPost();
         postStore.posts = [...posts];
     } catch (error: any) {
-        //ネットワークエラーのerro.vueを表示
-        throw createError({ statusCode: 500, statusMessage: 'ネットワークエラーが発生しました。', fatal: true })
-
+        //400,500番台の場合はエラーアラートを表示
+        if (error.message) {
+            state.showAlertFlag = true;
+            state.alertMessage = error.message;
+        } else {
+            //ネットワークエラーのerro.vueを表示
+            throw createError({ statusCode: 500, statusMessage: 'ネットワークエラーが発生しました。', fatal: true })
+        }
     }
 })
 </script>
 
 <template>
-    <UContainer class="h-screen flex justify-center items-center">
+    <UContainer class="h-screen flex-col flex justify-center items-center">
+        <UAlert  v-if="state.showAlertFlag" color="error" class="w-1/2" :title=state.alertMessage />
         <div class="h-full w-1/2 flex-col items-center justify-items-center hidden-scrollbar bg-white">
             <Post v-for="post in postStore.getPosts" :post="post"></Post>
         </div>
