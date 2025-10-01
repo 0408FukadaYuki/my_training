c225d9bf774b61ad2bb7c0cb5423e2a365636747
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-10-01 14:08:18 +0900

    投稿コンポーネントを新規作成

---
+++ frontend/app/components/Post.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+interface Props {
+    post: Post,
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+    <UCard class="border-1 rounded-none mt-0.5 w-full">
+        <template #header>
+            <div class="h-8 flex">
+                <UAvatar src="/img/exampleImage.png" size="sm" />
+                <div class="ml-1">{{ post.userName }}</div>
+                <div class="ml-1 text-gray-400">@{{ post.userId }}</div>
+            </div>
+        </template>
+
+        <div class="h-16 ">
+            {{ post.content }}
+        </div>
+
+        <template #footer>
+            <div class="h-8 flex ">
+                <UButton>リプライ</UButton>
+                <UButton>お気に入り</UButton>
+            </div>
+        </template>
+    </UCard>
+</template>
