8aa80bd610c49e533f63adb425ef3c025344f83e
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-10-01 14:11:18 +0900

    タイムライン画面の新規作成

--- frontend/app/pages/timeLine.vue
+++ frontend/app/pages/timeLine.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+const { getPost } = usePost()
+const postStore = usePostStore();
 
+onMounted(async () => {
+    try {
+        const posts: Post[] = await getPost();
+        postStore.posts = [...posts];
+    } catch (error: any) {
+        //ネットワークエラーのerro.vueを表示
+        throw createError({ statusCode: 500, statusMessage: 'ネットワークエラーが発生しました。', fatal: true })
+
+    }
+})
 </script>
 
 <template>
-    <UContainer class="bg-gray-200 h-screen flex justify-center items-center">
-        <div>
-            <h1>タイムライン画面</h1>
+    <UContainer class="h-screen flex justify-center items-center">
+        <div class="h-full w-1/2 flex-col items-center justify-items-center hidden-scrollbar bg-white">
+            <Post v-for="post in postStore.getPosts" :post="post"></Post>
         </div>
     </UContainer>
-
 </template>
