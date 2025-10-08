7b6567974063f316eb446e4bbb67bf776e8e955c
0408FukadaYuki <fukada.yuki.2e6@jp.nssol.nipponsteel.com>
2025-10-08 09:30:38 +0900

    PostテーブルとFavoriteテーブルを結合してポストの情報と指定したユーザーがお気に入りしているかを検索するカスタムクエリを作成

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,9 +1,20 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.Post;
+import com.example.demo.model.PostWithFavorite;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT new com.example.demo.model.PostWithFavorite(p, " +
+            "CASE WHEN f.user.id IS NOT NULL THEN true ELSE false END AS favorite) " +
+            "FROM Post p LEFT JOIN Favorite f ON p.id = f.post.id AND f.user.id = :userId " +
+            "ORDER BY p.createdAt DESC")
+    List<PostWithFavorite> findAllPost(@Param("userId") String userId);
+
 }
